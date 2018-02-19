oleewere/ambari

AMBARI-22842. Provide field / component UI metadata from backend

ambari-logsearch/ambari-logsearch-server/src/main/java/org/apache/ambari/logsearch/common/LabelFallbackHandler.java
@@ -1,0 +1,111 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.ambari.logsearch.common;
+
+import com.google.common.annotations.VisibleForTesting;
+import io.jsonwebtoken.lang.Collections;
+import org.apache.ambari.logsearch.conf.UIMappingConfig;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.text.WordUtils;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.List;
+
+@Named
+public class LabelFallbackHandler {
+
+  private final UIMappingConfig uiMappingConfig;
+
+  @Inject
+  public LabelFallbackHandler(UIMappingConfig uiMappingConfig) {
+    this.uiMappingConfig = uiMappingConfig;
+  }
+
+  public String fallbackIfRequired(String field, String label, boolean replaceUnderscore,
+                                   boolean replaceUppercaseInWord, boolean capitalizeAll) {
+    if (isEnabled() && StringUtils.isBlank(label)) {
+      return fallback(field,replaceUnderscore, replaceUppercaseInWord, capitalizeAll);
+    }
+    return label;
+  }
+
+  public String fallbackIfRequired(String field, String label, boolean replaceUnderscore,
+                                   boolean replaceUppercaseInWord, boolean capitalizeAll, List<String> prefixesToRemove) {
+    if (isEnabled() && StringUtils.isBlank(label)) {
+      return fallback(field,replaceUnderscore, replaceUppercaseInWord, capitalizeAll, prefixesToRemove);
+    }
+    return label;
+  }
+
+  public String fallback(String field, boolean replaceUnderscore, boolean replaceUppercaseInWord, boolean capitalizeAll) {
+    String result = null;
+    if (StringUtils.isNotBlank(field)) {
+     if (replaceUppercaseInWord) {
+       result = capitalize(deCamelCase(field), false);
+     }
+     if (replaceUnderscore) {
+       result = capitalize(deUnderScore(result != null ? result : field), capitalizeAll);
+     }
+    }
+    return result;
+  }
+
+  public String fallback(String field, boolean replaceUnderscore, boolean replaceUppercaseInWord, boolean capitalizeAll, List<String> prefixesToRemove) {
+    String fieldWithoutPrefix =  null;
+    if (!Collections.isEmpty(prefixesToRemove)) {
+      for (String prefix : prefixesToRemove) {
+        if (StringUtils.isNotBlank(field) && field.startsWith(prefix)) {
+          fieldWithoutPrefix = field.substring(prefix.length());
+        }
+      }
+    }
+    return fallback(fieldWithoutPrefix != null ? fieldWithoutPrefix : field, replaceUnderscore, replaceUppercaseInWord, capitalizeAll);
+  }
+
+  private String deUnderScore(String input) {
+    return input.replaceAll("_", " ");
+  }
+
+  private String capitalize(String input, boolean capitalizeAll) {
+    if (capitalizeAll) {
+      return WordUtils.capitalizeFully(input);
+    } else {
+      Character firstLetter = Character.toUpperCase(input.charAt(0));
+      return input.length() > 1 ? firstLetter + input.substring(1) : firstLetter.toString();
+    }
+  }
+
+  private String deCamelCase(String input) {
+    StringBuilder result = new StringBuilder();
+    for(int i=0 ; i < input.length() ; i++) {
+      char c = input.charAt(i);
+      if(i != 0 && Character.isUpperCase(c)) {
+        result.append(' ');
+      }
+      result.append(c);
+    }
+    return result.toString();
+  }
+
+  public boolean isEnabled() {
+    return uiMappingConfig.isLabelFallbackEnabled();
+  }
+
+}

ambari-logsearch/ambari-logsearch-server/src/main/java/org/apache/ambari/logsearch/common/PropertiesSplitter.java
@@ -1,0 +1,93 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.ambari.logsearch.common;
+
+import com.google.common.base.Splitter;
+import io.jsonwebtoken.lang.Collections;
+import org.apache.commons.lang.StringUtils;
+
+import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Named
+public class PropertiesSplitter {
+
+  public List<String> parseList(String listStr) {
+    return parseList(listStr, ",");
+  }
+
+  public Map<String, String> parseMap(String mapStr) {
+    return parseMap(mapStr, ",", ":");
+  }
+
+  private List<String> parseList(String listStr, String separator) {
+    return StringUtils.isNotBlank(listStr) ? Splitter.on(separator).splitToList(listStr) : new ArrayList<>();
+  }
+
+  public Map<String, String> parseMap(String mapStr, String separator, String keyValueSeparator) {
+    Map<String, String> resultMap = new HashMap<>();
+    if (StringUtils.isNotBlank(mapStr)) {
+      List<String> keyValueList = parseList(mapStr, separator);
+      if (!keyValueList.isEmpty()) {
+        for (String keyValueElement : keyValueList) {
+          if (StringUtils.isNotEmpty(keyValueElement)) {
+            List<String> keyValueElementList = parseList(keyValueElement, keyValueSeparator);
+            if (!Collections.isEmpty(keyValueElementList) && keyValueElementList.size() >= 2
+              && StringUtils.isNotBlank(keyValueElementList.get(0))) {
+              resultMap.put(keyValueElementList.get(0), keyValueElementList.get(1));
+            }
+          }
+        }
+      }
+    }
+    return resultMap;
+  }
+
+  public Map<String, Map<String, String>> parseMapInMap(String mapInMapStr) {
+    Map<String, Map<String, String>> mapInMap = new HashMap<>();
+    Map<String, String> outerMap = parseMap(mapInMapStr, ";", "#");
+    if (!outerMap.isEmpty()) {
+      for (Map.Entry<String, String> entry : outerMap.entrySet()) {
+        Map<String, String> keyValueMap = parseMap(entry.getValue());
+        if (!keyValueMap.isEmpty()) {
+          mapInMap.put(entry.getKey(), keyValueMap);
+        }
+      }
+    }
+    return mapInMap;
+  }
+
+  public Map<String, List<String>> parseListInMap(String listInMapStr) {
+    return parseListInMap(listInMapStr, ";", ":", ",");
+  }
+
+  public Map<String, List<String>> parseListInMap(String listInMapStr, String mapSeparator, String keyValueSeparator, String listSeparator) {
+    Map<String, List<String>> listInMap = new HashMap<>();
+    Map<String, String> typeKeyValueMap = parseMap(listInMapStr, mapSeparator, keyValueSeparator);
+    for (Map.Entry<String, String> entry : typeKeyValueMap.entrySet()) {
+      List<String> valuesList = parseList(entry.getValue(), listSeparator);
+      listInMap.put(entry.getKey(), valuesList);
+    }
+    return listInMap;
+  }
+
+}

ambari-logsearch/ambari-logsearch-server/src/main/java/org/apache/ambari/logsearch/solr/SolrConstants.java
@@ -57,6 +57,7 @@ public class SolrConstants {
     public static final String LOG_MESSAGE = "log_message";
     public static final String KEY_LOG_MESSAGE = "key_log_message";
     public static final String HOST = "host";
+    public static final String GROUP = "host";
     public static final String LEVEL = "level";
     public static final String THREAD_NAME = "thread_name";
     public static final String LOGGER_NAME = "logger_name";

apache/ambari-logsearch

ambari-logsearch-server/src/main/java/org/apache/ambari/logsearch/common/LabelFallbackHandler.java
@@ -1,0 +1,110 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.ambari.logsearch.common;
+
+import org.apache.ambari.logsearch.conf.UIMappingConfig;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.text.WordUtils;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.List;
+
+@Named
+public class LabelFallbackHandler {
+
+  private final UIMappingConfig uiMappingConfig;
+
+  @Inject
+  public LabelFallbackHandler(UIMappingConfig uiMappingConfig) {
+    this.uiMappingConfig = uiMappingConfig;
+  }
+
+  public String fallbackIfRequired(String field, String label, boolean replaceUnderscore,
+                                   boolean replaceUppercaseInWord, boolean capitalizeAll) {
+    if (isEnabled() && StringUtils.isBlank(label)) {
+      return fallback(field,replaceUnderscore, replaceUppercaseInWord, capitalizeAll);
+    }
+    return label;
+  }
+
+  public String fallbackIfRequired(String field, String label, boolean replaceUnderscore,
+                                   boolean replaceUppercaseInWord, boolean capitalizeAll, List<String> prefixesToRemove) {
+    if (isEnabled() && StringUtils.isBlank(label)) {
+      return fallback(field,replaceUnderscore, replaceUppercaseInWord, capitalizeAll, prefixesToRemove);
+    }
+    return label;
+  }
+
+  public String fallback(String field, boolean replaceUnderscore, boolean replaceUppercaseInWord, boolean capitalizeAll) {
+    String result = null;
+    if (StringUtils.isNotBlank(field)) {
+     if (replaceUppercaseInWord) {
+       result = capitalize(deCamelCase(field), false);
+     }
+     if (replaceUnderscore) {
+       result = capitalize(deUnderScore(result != null ? result : field), capitalizeAll);
+     }
+    }
+    return result;
+  }
+
+  public String fallback(String field, boolean replaceUnderscore, boolean replaceUppercaseInWord, boolean capitalizeAll, List<String> prefixesToRemove) {
+    String fieldWithoutPrefix =  null;
+    if (!CollectionUtils.isEmpty(prefixesToRemove)) {
+      for (String prefix : prefixesToRemove) {
+        if (StringUtils.isNotBlank(field) && field.startsWith(prefix)) {
+          fieldWithoutPrefix = field.substring(prefix.length());
+        }
+      }
+    }
+    return fallback(fieldWithoutPrefix != null ? fieldWithoutPrefix : field, replaceUnderscore, replaceUppercaseInWord, capitalizeAll);
+  }
+
+  private String deUnderScore(String input) {
+    return input.replaceAll("_", " ");
+  }
+
+  private String capitalize(String input, boolean capitalizeAll) {
+    if (capitalizeAll) {
+      return WordUtils.capitalizeFully(input);
+    } else {
+      Character firstLetter = Character.toUpperCase(input.charAt(0));
+      return input.length() > 1 ? firstLetter + input.substring(1) : firstLetter.toString();
+    }
+  }
+
+  private String deCamelCase(String input) {
+    StringBuilder result = new StringBuilder();
+    for(int i=0 ; i < input.length() ; i++) {
+      char c = input.charAt(i);
+      if(i != 0 && Character.isUpperCase(c)) {
+        result.append(' ');
+      }
+      result.append(c);
+    }
+    return result.toString();
+  }
+
+  public boolean isEnabled() {
+    return uiMappingConfig.isLabelFallbackEnabled();
+  }
+
+}

ambari-logsearch-server/src/main/java/org/apache/ambari/logsearch/common/PropertiesSplitter.java
@@ -1,0 +1,93 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.ambari.logsearch.common;
+
+import com.google.common.base.Splitter;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
+
+import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Named
+public class PropertiesSplitter {
+
+  public List<String> parseList(String listStr) {
+    return parseList(listStr, ",");
+  }
+
+  public Map<String, String> parseMap(String mapStr) {
+    return parseMap(mapStr, ",", ":");
+  }
+
+  private List<String> parseList(String listStr, String separator) {
+    return StringUtils.isNotBlank(listStr) ? Splitter.on(separator).splitToList(listStr) : new ArrayList<>();
+  }
+
+  public Map<String, String> parseMap(String mapStr, String separator, String keyValueSeparator) {
+    Map<String, String> resultMap = new HashMap<>();
+    if (StringUtils.isNotBlank(mapStr)) {
+      List<String> keyValueList = parseList(mapStr, separator);
+      if (!keyValueList.isEmpty()) {
+        for (String keyValueElement : keyValueList) {
+          if (StringUtils.isNotEmpty(keyValueElement)) {
+            List<String> keyValueElementList = parseList(keyValueElement, keyValueSeparator);
+            if (!CollectionUtils.isEmpty(keyValueElementList) && keyValueElementList.size() >= 2
+              && StringUtils.isNotBlank(keyValueElementList.get(0))) {
+              resultMap.put(keyValueElementList.get(0), keyValueElementList.get(1));
+            }
+          }
+        }
+      }
+    }
+    return resultMap;
+  }
+
+  public Map<String, Map<String, String>> parseMapInMap(String mapInMapStr) {
+    Map<String, Map<String, String>> mapInMap = new HashMap<>();
+    Map<String, String> outerMap = parseMap(mapInMapStr, ";", "#");
+    if (!outerMap.isEmpty()) {
+      for (Map.Entry<String, String> entry : outerMap.entrySet()) {
+        Map<String, String> keyValueMap = parseMap(entry.getValue());
+        if (!keyValueMap.isEmpty()) {
+          mapInMap.put(entry.getKey(), keyValueMap);
+        }
+      }
+    }
+    return mapInMap;
+  }
+
+  public Map<String, List<String>> parseListInMap(String listInMapStr) {
+    return parseListInMap(listInMapStr, ";", ":", ",");
+  }
+
+  public Map<String, List<String>> parseListInMap(String listInMapStr, String mapSeparator, String keyValueSeparator, String listSeparator) {
+    Map<String, List<String>> listInMap = new HashMap<>();
+    Map<String, String> typeKeyValueMap = parseMap(listInMapStr, mapSeparator, keyValueSeparator);
+    for (Map.Entry<String, String> entry : typeKeyValueMap.entrySet()) {
+      List<String> valuesList = parseList(entry.getValue(), listSeparator);
+      listInMap.put(entry.getKey(), valuesList);
+    }
+    return listInMap;
+  }
+
+}

ambari-logsearch-server/src/main/java/org/apache/ambari/logsearch/solr/SolrConstants.java
@@ -57,6 +57,7 @@ public class SolrConstants {
     public static final String LOG_MESSAGE = "log_message";
     public static final String KEY_LOG_MESSAGE = "key_log_message";
     public static final String HOST = "host";
+    public static final String GROUP = "group";
     public static final String LEVEL = "level";
     public static final String THREAD_NAME = "thread_name";
     public static final String LOGGER_NAME = "logger_name";